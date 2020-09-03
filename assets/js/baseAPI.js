f13f260a961ccfe71654f1bff181d61824112715
drk-rui-kai <70366456+drk-rui-kai@users.noreply.github.com>
2020-09-03 14:41:44 +0800

    index over

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -2,6 +2,25 @@
 // 会先调用 ajaxPrefilter 这个函数
 // 在这个函数中，可以拿到我们给Ajax提供的配置对象
 $.ajaxPrefilter(function(options) {
-  // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
-  options.url = 'http://ajax.frontend.itheima.net' + options.url
+    // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
+    options.url = 'http://ajax.frontend.itheima.net' + options.url
+
+    // 统一为有权限的接口，设置 headers 请求头
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+    // 全局统一挂戴 complete 回调函数
+    // 当请求结束后，判断用户的设置访问权限
+    options.complete = function(res) {
+        // 当用户身份认证失败后
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败') {
+            // 1.强制清空本地 token
+            localStorage.removeItem('token')
+                // 2.强制跳转到登录页面
+            location.href = '/login.html'
+        }
+    }
 })
